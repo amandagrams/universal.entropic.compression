06a201b2a8c2d2ec9f531d31f21290b58ec733a1
Amanda Grams <amanda.grams@gmail.com>
2020-08-18 08:10:23 -0300

    Created a command line project

---
+++ src/universalentropiccompression/universal.entropic.compression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace universal.entropic.compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
